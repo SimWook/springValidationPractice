66ce5632ae697e2150853d38fb60c62dd7bff5aa
シム ウク <astralkaiser89@gmail.com>
2023-11-30 16:59:40 +0900

    validation practice studying complete

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,11 +3,21 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "総計が10000ウォン以上入力してください")
 public class Item {
 
+    //@NotNull(groups=UpdateCheck.class)
     private Long id;
+
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 100000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value=9999,groups=SaveCheck.class)
     private Integer quantity;
 
     public Item() {
